6a577bad9f5d4564342b45e0b00d64b04294944e
khaledrefaat <khaledrefaat08@gmail.com>
2021-02-21 04:02:02 +0200

    shop page and collectionpreview done

---
+++ src/componenets/CollectionPreview.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+import './CollectionPreview.scss';
+
+function CollectionPreview({ title, len, items }) {
+  function renderCollections() {
+    if (len) {
+      return (
+        <Row>
+          <Col lg="12">
+            <h2 className="shop-title">{title}</h2>
+          </Col>
+          {items
+            .filter((item, index) => index < len)
+            .map(item => (
+              <Col key={item.id} lg="3" sm="6" xs="12">
+                <Card className="shop-card">
+                  <div className="img-container">
+                    <Card.Img variant="top" src={item.imageUrl} />
+                  </div>
+                  <Card.Body>
+                    <div className="card-content">
+                      <h3>{item.name}</h3>
+                      <h3>{item.price}$</h3>
+                    </div>
+                    <button className="card-button">add to cart</button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+        </Row>
+      );
+    }
+    return (
+      <Row>
+        <Col lg="12">{title}</Col>
+        {items.map(item => (
+          <Col key={item.id} lg="3" sm="6" xs="12">
+            <Card className="shop-card">
+              <div className="img-container">
+                <Card.Img variant="top" src={item.imageUrl} />
+              </div>
+              <Card.Body>
+                <div className="card-content">
+                  <h3>{item.name}</h3>
+                  <h3>{item.price}$</h3>
+                </div>
+                <button className="card-button">add to cart</button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    );
+  }
+
+  return renderCollections();
+}
+
+export default CollectionPreview;
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import shopData from './shopData';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import CollectionPreview from '../componenets/CollectionPreview';
+import { Container } from 'react-bootstrap';
 
 import './Shop.scss';
 
@@ -10,82 +11,13 @@ export class Shop extends Component {
   };
 
   render() {
-    const { collections } = this.state;
-    collections.forEach(({ id, ...others }) => console.log(id, others.items));
-    // console.log(...collections);
+    console.log(this.state.collections);
     return (
-      <Container fluid>
-        <Row>
-          <Col lg="12">Shoes</Col>
-          <Col lg="3" sm="6" xs="12">
-            <Col lg="12">
-              <Card className="shop-card">
-                <Card.Img
-                  variant="top"
-                  src="https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png"
-                />
-                <Card.Body>
-                  <div className="card-content">
-                    <h3>Brown Brim</h3>
-                    <h3>35$</h3>
-                  </div>
-                  <button className="card-button">add to cart</button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Col>
-          <Col lg="3" sm="6" xs="12">
-            <Col lg="12">
-              <Card className="shop-card">
-                <Card.Img
-                  variant="top"
-                  src="https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png"
-                />
-                <Card.Body>
-                  <div className="card-content">
-                    <h3>Brown Brim</h3>
-                    <h3>35$</h3>
-                  </div>
-                  <button className="card-button">add to cart</button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Col>
-          <Col lg="3" sm="6" xs="12">
-            <Col lg="12">
-              <Card className="shop-card">
-                <Card.Img
-                  variant="top"
-                  src="https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png"
-                />
-                <Card.Body>
-                  <div className="card-content">
-                    <h3>Brown Brim</h3>
-                    <h3>35$</h3>
-                  </div>
-                  <button className="card-button">add to cart</button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Col>
-          <Col lg="3" sm="6" xs="12">
-            <Col lg="12">
-              <Card className="shop-card">
-                <Card.Img
-                  variant="top"
-                  src="https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png"
-                />
-                <Card.Body>
-                  <div className="card-content">
-                    <h3>Brown Brim</h3>
-                    <h3>35$</h3>
-                  </div>
-                  <button className="card-button">add to cart</button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Col>
-        </Row>
+      <Container fluid className="shop-container">
+        <h1>Collections</h1>
+        {this.state.collections.map(({ id, ...others }) => (
+          <CollectionPreview key={id} len="4" {...others} />
+        ))}
       </Container>
     );
   }
