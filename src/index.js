6dd4b4be6347542dce3d699073fd72974226cccd
khaledrefaat <khaledrefaat08@gmail.com>
2021-04-11 17:30:38 +0200

    storing cart in localStorage with reduxPersist

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/reducers/store';
+import { store, persistor } from './redux/reducers/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
--- src/redux/reducers/store.js
+++ src/redux/reducers/store.js
@@ -1,7 +1,13 @@
+import { persistStore } from 'redux-persist';
 import { createStore, applyMiddleware, compose } from 'redux';
 
 import rootReducer from './index';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(rootReducer, composeEnhancers(applyMiddleware()));
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware())
+);
+
+export const persistor = persistStore(store);
